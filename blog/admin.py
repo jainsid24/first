148493abf8e8c690d5425ccfc60260ba3ea9999a
nicx <jainsid24@gmail.com>
2018-02-06 03:39:37 -0500

    mark6

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import Post
 from django.contrib.admin.models import LogEntry
 
-LogEntry.objects.all().delete()
 # Register your models here.
 admin.site.register(Post)
